86d6ce70e9c8586a007ec863ea8b0778e96eec4c
nareshaspire <nareshaspire45@gmail.com>
2022-08-18 09:44:27 -0400

    Add project files.

---
+++ BookTrack/Data/BookTrackContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookTrack.Models;
+
+namespace BookTrack.Data
+{
+    public class BookTrackContext : DbContext
+    {
+        public BookTrackContext (DbContextOptions<BookTrackContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookTrack.Models.Book> Book { get; set; } = default!;
+    }
+}
